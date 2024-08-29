a726ba70cf1ac9f275ccba9d3c637c2f570a1568
treyhulse <treyhulse3@gmail.com>
2024-08-29 11:47:03 -0500

    Update 03_Marketing.py

--- pages/03_Marketing.py
+++ pages/03_Marketing.py
@@ -1,15 +1,47 @@
 import logging
 import streamlit as st
-from utils.mongo_connection import get_mongo_client, get_collection_data
+from pymongo import MongoClient
+import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder
 
-# Configure logging for the main page
+# Configure logging for the utility file
 logging.basicConfig(
     filename="app.log", 
     filemode="a", 
     format="%(asctime)s - %(levelname)s - %(message)s", 
     level=logging.DEBUG
 )
+
+def get_mongo_client():
+    try:
+        logging.debug("Attempting to connect to MongoDB...")
+        connection_string = st.secrets["mongo_connection_string"] + "?retryWrites=true&w=majority"
+        client = MongoClient(connection_string, 
+                     ssl=True,
+                     serverSelectionTimeoutMS=30000,
+                     connectTimeoutMS=30000,
+                     socketTimeoutMS=30000)
+        logging.info("MongoDB connection successful")
+        return client
+    except Exception as e:
+        logging.error(f"Failed to connect to MongoDB: {e}")
+        raise
+
+def get_collection_data(client, collection_name):
+    try:
+        logging.debug(f"Fetching data from collection: {collection_name}")
+        db = client['netsuite']  # Ensure the database name is correct
+        collection = db[collection_name]
+        data = pd.DataFrame(list(collection.find()))
+        
+        # Handling potential UTF-8 encoding issues
+        data = data.applymap(lambda x: x.encode('utf-8', errors='ignore').decode('utf-8') if isinstance(x, str) else x)
+
+        logging.info(f"Data fetched successfully from {collection_name}")
+        return data
+    except Exception as e:
+        logging.error(f"Error fetching data from collection {collection_name}: {e}")
+        raise
 
 def main():
     try:
@@ -18,7 +50,7 @@ def main():
         # Connect to MongoDB using the utility function
         client = get_mongo_client()
 
-        # Select the collection to display (in this case, always 'sales')
+        # Specify the collection to display (in this case, always 'sales')
         collection_name = "sales"
 
         # Get the data from the sales collection using the utility function
