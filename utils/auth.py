b4122e11ceec4a7412ddd7ffbe2cfa6e458e6865
treyhulse <treyhulse3@gmail.com>
2024-08-29 22:25:01 -0500

    Create auth.py

---
+++ utils/auth.py
@@ -0,0 +1,19 @@
+# auth.py
+
+roles = {
+    'Person': ['trey.hulse@kcstorefixtures.com', 'person2@example.com', 'user1@example.com'],
+    'Manager': ['trey.hulse@kcstorefixtures.com', 'manager2@example.com', 'user2@example.com'],
+    'Director': ['trey.hulse@kcstorefixtures.com', 'director2@example.com', 'user3@example.com'],
+}
+
+def has_role(email, role):
+    return email in roles.get(role, [])
+
+def has_any_role(email, role_list):
+    return any(has_role(email, role) for role in role_list)
+
+def get_user_role(email):
+    for role, emails in roles.items():
+        if email in emails:
+            return role
+    return None
