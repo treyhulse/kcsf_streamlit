d37681b073e040718f4e5d970ab5c866306363cc
treyhulse <treyhulse3@gmail.com>
2024-08-29 23:29:00 -0500

    imports

--- pages/06_Logistics.py
+++ pages/06_Logistics.py
@@ -35,35 +35,6 @@ st.write(f"You have access to the {user_role} tools on this page.")
 import pandas as pd
 import pydeck as pdk
 from datetime import datetime, timedelta
-from utils.auth import capture_user_email, validate_access, show_permission_violation, get_sidebar_content, get_user_role, validate_page_access
-
-# Capture the user's email
-user_email = capture_user_email()
-if user_email is None:
-    st.error("Unable to retrieve user information.")
-    st.stop()
-
-# Validate access to this specific page
-page_name = '01_Shipping Report.py'  # Adjust this based on the current page
-if not validate_page_access(user_email, page_name):
-    show_permission_violation()
-
-# Sidebar content based on role
-user_role = get_user_role(user_email)
-st.sidebar.title(f"{user_role} Tools")
-sidebar_content = get_sidebar_content(user_role)
-
-for item in sidebar_content:
-    st.sidebar.write(item)
-
-
-################################################################################################
-
-## AUTHENTICATED
-
-################################################################################################
-
-
 
 # Page title
 st.title("Logistics Insights")
