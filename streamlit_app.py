ca671fbec6008f0304b0bdc4db1f11eb1bded63b
treyhulse <treyhulse3@gmail.com>
2024-09-03 09:04:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,13 @@
 import streamlit as st
 
-# Set the page configuration with title and favicon
 st.set_page_config(page_title="NetSuite Data Analytics", 
                    page_icon="./assets/KCSF_Sqaure.png", layout="wide")
 
-# Add the logo to the top of the sidebar
-st.sidebar.image("./assets/kcsf_red.png", use_column_width=True)
-
-# Rename the title to 'Home' in the sidebar
-st.sidebar.markdown("## Home")
-
-# Add a success message below the title in the sidebar
-st.sidebar.success("Select a report above.")
-
-# Main content
 st.write("# Welcome to NetSuite Data Analytics! 👋")
 
-# Information about the available reports
+st.sidebar.image("./assets/kcsf_red.png", use_column_width=True)
+st.sidebar.success("Select a report above.")
+
 st.markdown(
     """
     This app provides various reports and analytics for NetSuite data.
@@ -32,6 +23,7 @@ st.markdown(
     - Logistics Insights (still working on this!)
     - AI Insights (still working on this!)
 
+    
     More reports coming soon!
     """
 )
