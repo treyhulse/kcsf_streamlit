547cbcc7b6c0012d6c9150f93e53d7d930ad22aa
treyhulse <treyhulse3@gmail.com>
2024-09-03 16:10:14 -0500

    Update 03_Marketing.py

--- pages/03_Marketing.py
+++ pages/03_Marketing.py
@@ -296,7 +296,7 @@ def main():
     client = get_mongo_client()
 
     # Load the 'sales' collection into a DataFrame
-    data = get_collection_data(client, 'sales')
+    data = get_collection_data(client, 'salesLines')
 
     # Apply global filters
     filtered_data = apply_global_filters(data)
