9cfab4069dcc4d696415cf07d313540c445fe12b
treyhulse <treyhulse3@gmail.com>
2024-09-03 16:01:31 -0500

    auth remove

--- pages/04_Sales.py
+++ pages/04_Sales.py
@@ -20,28 +20,6 @@ st.write(f"You have access to this page.")
 ## AUTHENTICATED
 
 ################################################################################################
-import streamlit as st
-from utils.auth import capture_user_email, validate_page_access, show_permission_violation
-
-# Capture the user's email
-user_email = capture_user_email()
-if user_email is None:
-    st.error("Unable to retrieve user information.")
-    st.stop()
-
-# Validate access to this specific page
-page_name = '04_Sales.py'  # Adjust this based on the current page
-if not validate_page_access(user_email, page_name):
-    show_permission_violation()
-
-st.write(f"You have access to this page.")
-
-
-################################################################################################
-
-## AUTHENTICATED
-
-################################################################################################
 
 import logging
 from pymongo import MongoClient
