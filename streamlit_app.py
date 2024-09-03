e939ccab9ac2be09d99d4ed0f1bd0fbb683179e8
treyhulse <treyhulse3@gmail.com>
2024-09-03 09:47:12 -0500

    st_pages toml

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,18 @@
 import streamlit as st
-from st_pages import Page, show_pages, add_page_title
+from st_pages import add_page_title, get_nav_from_toml
 
-# Set the page configuration with title and favicon
-st.set_page_config(page_title="NetSuite Data Analytics", 
-                   page_icon="./assets/KCSF_Sqaure.png", layout="wide")
+st.set_page_config(layout="wide")
 
-# Add the title and icon to the current page
-add_page_title()
+sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
 
-# Add the logo to the top of the sidebar
-st.sidebar.image("./assets/kcsf_red.png", use_column_width=True)
-
-# Specify what pages should be shown in the sidebar, and their titles and icons
-show_pages(
-    [
-        Page("streamlit_app.py", "Home", "🏠"),
-        Page("other_pages/page2.py", "Shipping Report", "📦"),
-        Page("other_pages/page3.py", "Supply Chain", "🔗"),
-        Page("other_pages/page4.py", "Marketing", "📈"),
-        Page("other_pages/page5.py", "Sales", "💼"),
-        Page("other_pages/page6.py", "Shop", "🏭"),
-        Page("other_pages/page7.py", "Logistics", "🚚"),
-        Page("other_pages/page8.py", "AI Insights", "🤖"),
-        Page("other_pages/page9.py", "Showcase", "🌟"),
-        Page("other_pages/page10.py", "Role Permissions", "🔒"),
-    ]
+nav = get_nav_from_toml(
+    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
 )
 
-# Add a success message below the title in the sidebar
-st.sidebar.success("Select a report above.")
+st.logo("logo.png")
 
-# Main content
-st.write("# Welcome to NetSuite Data Analytics! 👋")
+pg = st.navigation(nav)
 
-# Information about the available reports
-st.markdown(
-    """
-    This app provides various reports and analytics for NetSuite data.
-    
-    👈 Select a report from the sidebar to get started!
-    
-    ### Available Reports:
-    - Shipping Report
-    - Sales Dashboard (still working on this!)
-    - Supply Chain Insights (still working on this!)
-    - Financial Insights (still working on this!)
-    - Operations Insights (still working on this!)
-    - Logistics Insights (still working on this!)
-    - AI Insights (still working on this!)
+add_page_title(pg)
 
-    More reports coming soon!
-    """
-)
+pg.run()
