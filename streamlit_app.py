1a28bf4da7171ec8de15bcfcf19c222c87553c81
treyhulse <treyhulse3@gmail.com>
2024-09-03 08:56:17 -0500

    assets and favicon

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="NetSuite Data Analytics", page_icon="📊", layout="wide")
+st.set_page_config(page_title="NetSuite Data Analytics", 
+                   page_icon="./assets/kcsf_red.png", layout="wide")
 
 st.write("# Welcome to NetSuite Data Analytics! 👋")
 
