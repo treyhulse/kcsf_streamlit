3801e2d2ee19efde6b3b20b6838dbed23a886217
treyhulse <treyhulse3@gmail.com>
2024-09-04 13:17:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
-st.set_page_config(page_title="NetSuite Data Analytics", 
-                   page_icon="./assets/KCSF_Square.png", 
-                   layout="wide")
+st.set_page_config(
+    page_title="NetSuite Data Analytics",
+    page_icon="./assets/KCSF_Square.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://kcstorefixtures.streamlit.app/',
+        'Report a bug': 'https://www.kc-store-fixtures.com/bug',
+        'About': "This is a dashboard and chart management tool."
+    }
+)
 
 st.write("# Welcome to NetSuite Data Analytics! 👋")
 
