5a1289f25a5d8f83e0146193d5dcef9d1561c6d2
treyhulse <treyhulse3@gmail.com>
2024-09-05 16:47:57 -0500

    Update shopify_connection.py

--- utils/shopify_connection.py
+++ utils/shopify_connection.py
@@ -1,11 +1,10 @@
 import requests
 import streamlit as st
 
-# Function to get the Shopify connection headers
-def get_shopify_headers():
-    api_key = st.secrets['shopify_api_key']
-    api_secret = st.secrets['shopify_api_secret']
-
+# Function to get the Shopify connection headers with the admin API key
+def get_shopify_headers(use_admin_key=False):
+    api_key = st.secrets['shopify_admin_api_key'] if use_admin_key else st.secrets['shopify_api_key']
+    
     headers = {
         "Content-Type": "application/json",
         "X-Shopify-Access-Token": api_key
@@ -17,7 +16,7 @@ def get_shopify_headers():
 def test_shopify_connection():
     shop_url = f"https://{st.secrets['shopify_store']}.myshopify.com/admin/api/2023-01/shop.json"
     
-    headers = get_shopify_headers()
+    headers = get_shopify_headers(use_admin_key=True)  # You can choose to use the admin key here
     
     response = requests.get(shop_url, headers=headers)
     
