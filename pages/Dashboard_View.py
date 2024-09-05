f739f225c37f8114b6bdd35715b3e1393ed7a0e7
treyhulse <treyhulse3@gmail.com>
2024-09-05 13:45:37 -0500

    big auth update - remove roles and page_access json

--- pages/Dashboard_View.py
+++ pages/Dashboard_View.py
@@ -1,4 +1,28 @@
 import streamlit as st
+from utils.auth import capture_user_email, validate_page_access, show_permission_violation
+
+# Capture the user's email
+user_email = capture_user_email()
+if user_email is None:
+    st.error("Unable to retrieve user information.")
+    st.stop()
+
+# Validate access to this specific page
+page_name = 'Dashboard_View.py'  # Adjust this based on the current page
+if not validate_page_access(user_email, page_name):
+    show_permission_violation()
+
+
+st.write(f"You have access to this page.")
+
+
+################################################################################################
+
+## AUTHENTICATED
+
+################################################################################################
+
+
 import plotly.express as px
 from pymongo import MongoClient
 import pandas as pd
