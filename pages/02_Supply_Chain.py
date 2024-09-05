3a67c5c04cb028e35fbcad8bb98f169075df10d5
treyhulse <treyhulse3@gmail.com>
2024-09-05 14:51:58 -0500

    page names 1

--- pages/02_Supply_Chain.py
+++ pages/02_Supply_Chain.py
@@ -8,7 +8,7 @@ if user_email is None:
     st.stop()
 
 # Validate access to this specific page
-page_name = '02_Supply_Chain.py'  # Adjust this based on the current page
+page_name = 'Supply Chain'  # Adjust this based on the current page
 if not validate_page_access(user_email, page_name):
     show_permission_violation()
 
