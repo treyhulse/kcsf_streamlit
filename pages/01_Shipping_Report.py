ce622295b93e5bd0d829a51a94ec47a78d945322
treyhulse <treyhulse3@gmail.com>
2024-09-06 16:08:30 -0500

    Update 01_Shipping_Report.py

--- pages/01_Shipping_Report.py
+++ pages/01_Shipping_Report.py
@@ -141,11 +141,11 @@ def get_date_range(preset):
 def main():
     with st.spinner("Fetching Open Sales Orders..."):
         df_open_so = process_netsuite_data_csv(st.secrets["url_open_so"])
-        st.write(df_open_so.columns)  # This will print the columns to the app UI
 
     with st.spinner("Fetching RF Pick Data..."):
         df_rf_pick = process_netsuite_data_csv(st.secrets["url_rf_pick"])
-
+    
+    st.write(df_rf_pick.columns)  # This will print the columns to the app UI
     # Map the 'Ship Via' and 'Terms' columns using the defined dictionaries
     df_open_so['Ship Via'] = df_open_so['Ship Via'].map(ship_via_mapping).fillna('Unknown')
     df_open_so['Terms'] = df_open_so['Terms'].map(terms_mapping).fillna('Unknown')
