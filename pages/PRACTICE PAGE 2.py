fd3a0c1a97f1294e047be95c6b1728366782c487
treyhulse <treyhulse3@gmail.com>
2024-09-13 02:02:48 -0500

    Update PRACTICE PAGE 2.py

--- pages/PRACTICE PAGE 2.py
+++ pages/PRACTICE PAGE 2.py
@@ -23,31 +23,6 @@ st.write(f"You have access to this page.")
 
 ################################################################################################
 
-import streamlit as st
-from utils.auth import capture_user_email, validate_page_access, show_permission_violation
-
-st.set_page_config(layout="wide")
-
-# Capture the user's email
-user_email = capture_user_email()
-if user_email is None:
-    st.error("Unable to retrieve user information.")
-    st.stop()
-
-# Validate access to this specific page
-page_name = 'Practice Page'  # Adjust this based on the current page
-if not validate_page_access(user_email, page_name):
-    show_permission_violation()
-
-
-st.write(f"You have access to this page.")
-
-################################################################################################
-
-## AUTHENTICATED
-
-################################################################################################
-
 import pandas as pd
 import plotly.express as px
 from utils.data_functions import process_netsuite_data_csv
