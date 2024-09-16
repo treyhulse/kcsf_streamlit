f77a15d8a635570aee7e8270ba370184ac341e86
treyhulse <treyhulse3@gmail.com>
2024-09-16 16:28:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,25 +7,6 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
-# Capture the user's email
-user_email = capture_user_email()
-if user_email is None:
-    st.error("Unable to retrieve user information.")
-    st.stop()
-
-# Validate access to this specific page
-page_name = 'Shipping Report'  # Adjust this based on the current page
-if not validate_page_access(user_email, page_name):
-    show_permission_violation()
-
-
-st.write(f"You have access to this page.")
-
-################################################################################################
-
-## AUTHENTICATED
-
-################################################################################################
 
 st.write("# Welcome to KC Store Fixtures! 👋")
 
