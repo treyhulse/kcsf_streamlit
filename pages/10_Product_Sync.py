f9b2d42097c6ecaf2c9edfea27877666e1177afb
treyhulse <treyhulse3@gmail.com>
2024-09-16 16:30:47 -0500

    Update 10_Product_Sync.py

--- pages/10_Product_Sync.py
+++ pages/10_Product_Sync.py
@@ -1,5 +1,29 @@
-# 10_Product_Sync.py
 import streamlit as st
+from utils.auth import capture_user_email, validate_page_access, show_permission_violation
+
+st.set_page_config(layout="wide")
+
+# Capture the user's email
+user_email = capture_user_email()
+if user_email is None:
+    st.error("Unable to retrieve user information.")
+    st.stop()
+
+# Validate access to this specific page
+page_name = 'Product Sync'  # Adjust this based on the current page
+if not validate_page_access(user_email, page_name):
+    show_permission_violation()
+
+
+st.write(f"You have access to this page.")
+
+################################################################################################
+
+## AUTHENTICATED
+
+################################################################################################
+
+
 from utils.apis import get_netsuite_products_via_restlet, get_shopify_products, post_product_to_shopify, update_inventory_and_price
 
 st.title("NetSuite & Shopify Product Sync")
