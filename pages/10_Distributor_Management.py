b3aabea1043b507748ae8fea4f993620c0383f69
treyhulse <treyhulse3@gmail.com>
2024-09-19 11:35:50 -0500

    DMS addition

---
+++ pages/10_Distributor_Management.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from utils.auth import capture_user_email, validate_page_access, show_permission_violation
+
+st.set_page_config(layout="wide")
+
+# Capture the user's email
+user_email = capture_user_email()
+if user_email is None:
+    st.error("Unable to retrieve user information.")
+    st.stop()
+
+# Validate access to this specific page
+page_name = 'Product Sync'  # Adjust this based on the current page
+if not validate_page_access(user_email, page_name):
+    show_permission_violation()
+
+
+st.write(f"You have access to this page.")
+
+
+
+################################################################################################
+
+## AUTHENTICATED
+
+################################################################################################
+
+
