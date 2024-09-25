62987b25294edc43640738f6150830dacdf03e3c
treyhulse <treyhulse3@gmail.com>
2024-09-25 14:06:16 -0500

    Update 04_Sales.py

--- pages/04_Sales.py
+++ pages/04_Sales.py
@@ -42,19 +42,22 @@ from kpi.sales_by_month import get_sales_by_month
 st.title("Sales Dashboard")
 
 # Display Sales by Rep
-st.subheader("Sales by Rep")
+st.markdown("[Sales by Rep](#sales-by-rep)")
+st.markdown("### <a name='sales-by-rep'></a>Sales by Rep", unsafe_allow_html=True)
 sales_by_rep_chart = get_sales_by_rep()
 if sales_by_rep_chart:
     st.plotly_chart(sales_by_rep_chart)
 
 # Display Sales by Category
-st.subheader("Sales by Category")
+st.markdown("[Sales by Category](#sales-by-category)")
+st.markdown("### <a name='sales-by-category'></a>Sales by Category", unsafe_allow_html=True)
 sales_by_category_chart = get_sales_by_category()
 if sales_by_category_chart:
     st.plotly_chart(sales_by_category_chart)
 
 # Display Sales by Month
-st.subheader("Sales by Month")
+st.markdown("[Sales by Month](https://3429264.app.netsuite.com/app/common/search/searchresults.nl?searchid=5146&whence=)")
+st.markdown("### <a name='sales-by-month'></a>Sales by Month", unsafe_allow_html=True)
 sales_by_month_chart = get_sales_by_month()
 if sales_by_month_chart:
     st.plotly_chart(sales_by_month_chart)
