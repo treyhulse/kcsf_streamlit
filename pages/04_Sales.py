2361b83d86d3e86c4d18280d778604a50add622d
treyhulse <treyhulse3@gmail.com>
2024-09-25 14:09:19 -0500

    Update 04_Sales.py

--- pages/04_Sales.py
+++ pages/04_Sales.py
@@ -41,23 +41,34 @@ from kpi.sales_by_month import get_sales_by_month
 
 st.title("Sales Dashboard")
 
-# Display Sales by Rep
-st.markdown("[Sales by Rep](#sales-by-rep)")
-st.markdown("### <a name='sales-by-rep'></a>Sales by Rep", unsafe_allow_html=True)
-sales_by_rep_chart = get_sales_by_rep()
-if sales_by_rep_chart:
-    st.plotly_chart(sales_by_rep_chart)
+# Create three columns for the visualizations
+col1, col2, col3 = st.columns(3)
 
-# Display Sales by Category
-st.markdown("[Sales by Category](#sales-by-category)")
-st.markdown("### <a name='sales-by-category'></a>Sales by Category", unsafe_allow_html=True)
-sales_by_category_chart = get_sales_by_category()
-if sales_by_category_chart:
-    st.plotly_chart(sales_by_category_chart)
+# Column 1: Sales by Rep
+with col1:
+    st.markdown("### Sales by Rep")
+    sales_by_rep_chart = get_sales_by_rep()
+    if sales_by_rep_chart:
+        st.plotly_chart(sales_by_rep_chart, use_container_width=True)
+    st.markdown("[Drill Down](https://3429264.app.netsuite.com/app/common/search/searchresults.nl?searchid=4963&whence=)")
 
-# Display Sales by Month
-st.markdown("[Sales by Month](https://3429264.app.netsuite.com/app/common/search/searchresults.nl?searchid=5146&whence=)")
-st.markdown("### <a name='sales-by-month'></a>Sales by Month", unsafe_allow_html=True)
-sales_by_month_chart = get_sales_by_month()
-if sales_by_month_chart:
-    st.plotly_chart(sales_by_month_chart)
+# Column 2: Sales by Category
+with col2:
+    st.markdown("### Sales by Category")
+    sales_by_category_chart = get_sales_by_category()
+    if sales_by_category_chart:
+        st.plotly_chart(sales_by_category_chart, use_container_width=True)
+    st.markdown("[Drill Down](https://3429264.app.netsuite.com/app/common/search/searchresults.nl?searchid=5145&whence=)")
+
+# Column 3: Sales by Month
+with col3:
+    st.markdown("### Sales by Month")
+    sales_by_month_chart = get_sales_by_month()
+    if sales_by_month_chart:
+        st.plotly_chart(sales_by_month_chart, use_container_width=True)
+    st.markdown("[Drill Down](https://3429264.app.netsuite.com/app/common/search/searchresults.nl?searchid=5146&whence=)")
+
+# Anchors for each section to allow drill-down
+st.markdown("<a name='sales-by-rep'></a>", unsafe_allow_html=True)
+st.markdown("<a name='sales-by-category'></a>", unsafe_allow_html=True)
+st.markdown("<a name='sales-by-month'></a>", unsafe_allow_html=True)
