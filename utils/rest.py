1ff94c24963c886ed7799b1d4bf3db7ea440fba5
treyhulse <treyhulse3@gmail.com>
2024-09-24 23:04:06 -0500

    Update rest.py

--- utils/rest.py
+++ utils/rest.py
@@ -3,13 +3,22 @@ from requests_oauthlib import OAuth1
 import streamlit as st
 
 def get_netsuite_auth():
-    """Authenticate using OAuth 1.0 with credentials from st.secrets"""
+    """Authenticate using OAuth 1.0 with credentials from st.secrets and include realm"""
     consumer_key = st.secrets["consumer_key"]
     consumer_secret = st.secrets["consumer_secret"]
     token_key = st.secrets["token_key"]
     token_secret = st.secrets["token_secret"]
+    realm = st.secrets["realm"]
 
-    return OAuth1(consumer_key, consumer_secret, token_key, token_secret, signature_method='HMAC-SHA256')
+    # Include the realm in the OAuth1 header
+    return OAuth1(
+        client_key=consumer_key,
+        client_secret=consumer_secret,
+        resource_owner_key=token_key,
+        resource_owner_secret=token_secret,
+        realm=realm,
+        signature_method='HMAC-SHA256'
+    )
 
 def make_netsuite_rest_api_request(endpoint):
     """Make an authenticated GET request to the NetSuite REST API"""
