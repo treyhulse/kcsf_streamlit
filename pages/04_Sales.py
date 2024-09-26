2cf196f113421385aeec00c541e6ed735cacadb6
treyhulse <treyhulse3@gmail.com>
2024-09-25 20:17:15 -0500

    Update 04_Sales.py

--- pages/04_Sales.py
+++ pages/04_Sales.py
@@ -77,7 +77,7 @@ if df_grouped is not None:
     st.markdown("""
     <style>
     .metrics-box {
-        background-color: #f9f9f9;
+        background-color: #262730;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 2px 2px 15px rgba(0, 0, 0, 0.1);
