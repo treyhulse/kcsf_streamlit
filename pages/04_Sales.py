9015e214d658dbae7d66628e214e241e883e9712
treyhulse <treyhulse3@gmail.com>
2024-09-30 15:27:42 -0500

    Update 04_Sales.py

--- pages/04_Sales.py
+++ pages/04_Sales.py
@@ -221,7 +221,7 @@ with tab2:
                 <p class="metric-change" style="color:{color};">{arrow} {metric['change']:.2f}%</p>
             </div>
             """, unsafe_allow_html=True)
-
+    st.write("")
     # Display Website revenue chart and data
     st.plotly_chart(chart_website_revenue_by_month, use_container_width=True)
     with st.expander("Data - Website Revenue by Month"):
@@ -244,7 +244,7 @@ with tab2:
                 <p class="metric-change" style="color:{color};">{arrow} {metric['change']:.2f}%</p>
             </div>
             """, unsafe_allow_html=True)
-
+    st.write("")
     # Display Amazon revenue chart and data
     st.plotly_chart(chart_amazon_sales_by_month, use_container_width=True)
     with st.expander("Data - Amazon Revenue by Month"):
