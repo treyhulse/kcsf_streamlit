e8f10c8897b9832f3879fabc8df3dbaa7f321fcb
treyhulse <treyhulse3@gmail.com>
2024-10-04 09:11:47 -0500

    Update 06_Order_Management.py

--- pages/06_Order_Management.py
+++ pages/06_Order_Management.py
@@ -47,7 +47,7 @@ import plotly.express as px
 
 
 # Cache the raw data fetching process, reset cache every 15 minutes (900 seconds)
-@st.cache_data(ttl=1200)
+@st.cache_data(ttl=600)
 def fetch_raw_data(saved_search_id):
     # Fetch raw data from RESTlet without filters
     df = fetch_restlet_data(saved_search_id)
