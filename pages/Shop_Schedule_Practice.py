3addbce48ed839f596e730bed2a40269f53f5c23
treyhulse <treyhulse3@gmail.com>
2024-10-04 16:57:37 -0500

    Update Shop_Schedule_Practice.py

--- pages/Shop_Schedule_Practice.py
+++ pages/Shop_Schedule_Practice.py
@@ -3,7 +3,7 @@ import pandas as pd
 import altair as alt
 from utils.restlet import fetch_restlet_data
 # Set the page title
-st.title("Manufacturing Insights")
+st.title("Distributor Management System")
 
 # Cache the raw data fetching process with a 10-minute expiration
 @st.cache_data(ttl=600)
@@ -14,6 +14,9 @@ def fetch_raw_data_with_progress(saved_search_id):
     # Simulating the data loading process in chunks
     df = fetch_restlet_data(saved_search_id)
     progress_bar.progress(33)  # 33% done after fetching data
+    progress_bar.progress(100)  # 100% done
+    progress_bar.empty()  # Remove progress bar when done
+    return df
 
 # Fetch raw data for customsearch5135 with progress bar
-customsearch5135_data_raw = fetch_raw_data_with_progress("customsearch5127")
+customsearch5135_data_raw = fetch_raw_data_with_progress("customsearch5162")
