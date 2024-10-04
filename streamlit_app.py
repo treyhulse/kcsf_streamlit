2032312ac147ee9b4e7e5edb368b0313c0899ed9
treyhulse <treyhulse3@gmail.com>
2024-10-04 10:24:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,13 +40,13 @@ For now, see below for tips & tricks.
 # Add a Tips & Tricks section
 st.header("Tips & Tricks")
 st.write("""
-- **Navigation**: Use the sidebar on the left to switch between different modulpageses.
-- **Filtering**: Utilize the filtering options within each module to view specific data from within the sidebar.
-- **Extracting Data**: Download CSVs for any filtered or displayed data for further analysis in the top right of any table.
-- **Searching Data**: Use the interactive search mechanism in the top right of tables to find specific order numbers, customers, ship methods, etc.
-- **Data Refresh**: Most data in here has a cache mechanism setup where it is refreshed every 15-20 minutes. I can work on adding a refresh button to update the data. The cache is too prevent recurring load times.
-- **Regular Updates**: Keep an eye out for new features and improvements to the existing modules. If the app is rebooting, you'll get a white screen with a cake icon in the middle. This reboot takes anywhere from 10 to 60 seconds.
+- **Navigation**: Use the sidebar on the left to switch between different pages.
+- **Filtering**: Utilize the filtering options within the sidebar to view specific data. (I'm working on a pre-filter so that when you view a page, it defaults to being filtered to just you.)
+- **Extracting Data**: You can download CSVs for any filtered or displayed data for further analysis or to send out from the download icon in the top right of any table.
+- **Searching Data**: Use the interactive search mechanism in the top right of tables to find specific order numbers, customers, ship methods, etc. Anything in the table should be searchable.
+- **Data Refresh**: Most data in here has a cache mechanism setup where it is refreshed every 15-20 minutes. I can work on adding a refresh button to update the data as needed. The cache is too prevent recurring load times.
+- **Regular Updates**: Keep an eye out for new features and improvements to the existing pages. If the app is rebooting, you'll get a white screen with a cake icon in the middle. This reboot takes anywhere from 10 seconds to 60 seconds.
 """)
 
-st.info("This is a work in progress, and your feedback is valuable to improve the app. Feel free to reach out!")
+st.info("This is a work in progress, and your feedback is valuable to improve the app. Please reach out!")
 
