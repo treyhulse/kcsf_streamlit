1eb90b0b7710d19f221bfd3ede8bbc62b4ff3f5d
treyhulse <treyhulse3@gmail.com>
2024-10-04 16:13:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,16 @@ st.set_page_config(
     layout="wide",
 )
 
+# Custom CSS to hide the top bar and footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # Custom CSS for card styling
 card_style = """
     <style>
