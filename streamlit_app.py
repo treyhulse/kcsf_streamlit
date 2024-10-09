9745c6de6819ef0e4200bbe615b8ec736ce0d82c
treyhulse <treyhulse3@gmail.com>
2024-10-09 10:08:44 -0500

    setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,16 +9,18 @@ pages = {
 
         st.Page("pages/Supply_Chain.py", title="Supply Chain", icon="📦"),
         st.Page("pages/MRP.py", title="Material Resource Planning", icon="🗓️"),
-        st.Page("pages/Shop_Schedule.py", title="Shop", icon="🛠️"),
+        st.Page("pages/Shop_Schedule.py", title="Shop Schedule", icon="🛠️"),
 
         st.Page("pages/Sales.py", title="Sales Dashboard", icon="💲"),
         st.Page("pages/Order_Management.py", title="Order Management", icon="💰"),
         st.Page("pages/Distributor_Management.py", title="Distributor Management", icon="👥"),
-
         st.Page("pages/Customer_Portal.py", title="Customer Portal", icon="👤"),
         st.Page("pages/Customer_Support.py", title="Support", icon="📞"),
         st.Page("pages/AI_Insights.py", title="AI Insights", icon="💬"),
         st.Page("pages/Role_and_Permissions.py", title="Role and Permissions", icon="🔒"),
+    ],
+    
+    "Practice": [  # Main section for general pages
         st.Page("pages/Pagination_Example.py", title="Pagination Example", icon="📄"),
         st.Page("pages/Practice_Page.py", title="Practice Page", icon="🌱"),
         st.Page("pages/MRP_TEST.py", title="MRP TEST", icon="🌱"),
