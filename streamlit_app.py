3109befe74cae75535812781c8f1a39a0a098be1
treyhulse <treyhulse3@gmail.com>
2024-10-10 14:12:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,25 @@ import streamlit as st
 # Define navigation pages with sections (using function names or file paths)
 pages = {
     "Main": [  # Main section for general pages
-        st.Page("pages/Home.py", title="Home", icon="🏠"),
-        st.Page("pages/Shipping_Report.py", title="Shipping Report", icon="🚚"),
-        st.Page("pages/TMS.py", title="Transportation Management System", icon="🌐"),
+        st.Page("pages/Home.py", title="Home"),
+        st.Page("pages/Shipping_Report.py", title="Shipping Report"),
+        st.Page("pages/TMS.py", title="Transportation Management System"),
 
-        st.Page("pages/Supply_Chain.py", title="Supply Chain", icon="📦"),
-        st.Page("pages/MRP.py", title="Material Resource Planning", icon="🗓️"),
-        st.Page("pages/Shop_Schedule.py", title="Shop Schedule", icon="🛠️"),
+        st.Page("pages/Supply_Chain.py", title="Supply Chain"),
+        st.Page("pages/MRP.py", title="Material Resource Planning"),
+        st.Page("pages/Shop_Schedule.py", title="Shop Schedule"),
 
-        st.Page("pages/Sales.py", title="Sales Dashboard", icon="💲"),
-        st.Page("pages/Order_Management.py", title="Order Management", icon="💰"),
-        st.Page("pages/Distributor_Management.py", title="Distributor Management", icon="👥"),
-        st.Page("pages/Customer_Portal.py", title="Customer Portal", icon="👤"),
-        st.Page("pages/Customer_Support.py", title="Support", icon="📞"),
-        st.Page("pages/AI_Insights.py", title="AI Insights", icon="💬"),
-        st.Page("pages/Roles_and_Permissions.py", title="Roles and Permissions", icon="🔒"),
+        st.Page("pages/Sales.py", title="Sales Dashboard"),
+        st.Page("pages/Order_Management.py", title="Order Management"),
+        st.Page("pages/Distributor_Management.py", title="Distributor Management"),
+        st.Page("pages/Customer_Portal.py", title="Customer Portal"),
+        st.Page("pages/Customer_Support.py", title="Support"),
+        st.Page("pages/AI_Insights.py", title="AI Insights"),
+        st.Page("pages/Roles_and_Permissions.py", title="Roles and Permissions"),
     ],
 
     "Practice": [  # Main section for general pages
-        st.Page("pages/Pagination_Example.py", title="Pagination Example", icon="📄"),
+        st.Page("pages/Pagination_Example.py", title="Pagination Example", icon="🌱"),
         st.Page("pages/Practice_Page.py", title="Practice Page", icon="🌱"),
         st.Page("pages/MRP_TEST.py", title="MRP TEST", icon="🌱"),
         st.Page("pages/Estes_Rate_Generator.py", title="Estes Rate Generator", icon="🌱"),
