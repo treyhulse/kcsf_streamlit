fe19eb1daf03e7069689eac4ad6fc7e3253c4930
treyhulse <treyhulse3@gmail.com>
2024-10-30 08:47:39 -0500

    Update Barcode_Generator.py

--- pages/Barcode_Generator.py
+++ pages/Barcode_Generator.py
@@ -1,4 +1,40 @@
 import streamlit as st
+from utils.auth import capture_user_email, validate_page_access, show_permission_violation
+
+st.set_page_config(page_title="Barcode Generator", 
+                   layout="wide",)
+
+# Custom CSS to hide the top bar and footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+# Capture the user's email
+user_email = capture_user_email()
+if user_email is None:
+    st.error("Unable to retrieve user information.")
+    st.stop()
+
+# Validate access to this specific page
+page_name = 'Practice Page'  # Adjust this based on the current page
+if not validate_page_access(user_email, page_name):
+    show_permission_violation()
+
+
+st.write(f"You have access to this page.")
+
+
+################################################################################################
+
+## AUTHENTICATED
+
+################################################################################################
+
 from barcode import Code128
 from barcode.writer import ImageWriter
 from PIL import Image
