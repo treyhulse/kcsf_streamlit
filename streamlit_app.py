ded935a32d6af4d2bfd57e35766a9262ad0fac4e
treyhulse <treyhulse3@gmail.com>
2025-02-07 09:02:23 -0600

    Cleaning up the pages here. Not usable.

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,28 +5,16 @@ pages = {
     "Main": [  # Main section for general pages
         st.Page("pages/Home.py", title="Home"),
         st.Page("pages/Shipping_Report.py", title="Shipping Report"),
-        st.Page("pages/TMS.py", title="Transportation Management System"),
 
         st.Page("pages/Supply_Chain.py", title="Supply Chain"),
-        st.Page("pages/MRP.py", title="Material Resource Planning"),
-        st.Page("pages/Shop_Schedule.py", title="Shop Schedule"),
 
-        st.Page("pages/Sales.py", title="Sales Dashboard"),
-        st.Page("pages/Order_Management.py", title="Order Management"),
-        st.Page("pages/Distributor_Management.py", title="Distributor Management"),
-        st.Page("pages/Customer_Portal.py", title="Customer Portal"),
-        st.Page("pages/Customer_Support.py", title="Support"),
-        st.Page("pages/AI_Insights.py", title="AI Insights"),
         st.Page("pages/Roles_and_Permissions.py", title="Roles and Permissions"),
         st.Page("pages/Estes_Rate_Generator.py", title="Estes Rate Generator", icon="🌱"),
         st.Page("pages/Barcode_Generator.py", title="Barcode Generator", icon="🌱"),
     ],
 
     "Practice": [  # Main section for general pages
-        st.Page("pages/Pagination_Example.py", title="Pagination Example", icon="🌱"),
         st.Page("pages/Practice_Page.py", title="Practice Page", icon="🌱"),
-        st.Page("pages/MRP_TEST.py", title="MRP TEST", icon="🌱"),
-        st.Page("pages/NVIDIA_Model.py", title="NVIDIA Model", icon="🌱"),
     ],
 }
 
