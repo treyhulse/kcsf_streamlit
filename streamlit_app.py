179c87a508f4833d73eb1ced7b552dbac2065635
treyhulse <treyhulse3@gmail.com>
2025-04-18 09:42:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,13 @@ pages = {
     "Main": [  # Main section for general pages
         st.Page("pages/Home.py", title="Home"),
         st.Page("pages/Shipping_Report.py", title="Shipping Report"),
-
-
         st.Page("pages/Roles_and_Permissions.py", title="Roles and Permissions"),
         st.Page("pages/Estes_Rate_Generator.py", title="Estes Rate Generator", icon="🌱"),
     ],
 }
 
 # Configure the sidebar navigation with sections
-pg = st.navigation(pages, position="sidebar", expanded=True)
+pg = st.navigation(pages, position="sidebar")
 
 # Run the selected page
 pg.run()
